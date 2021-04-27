f343f66a07ca6d98d88884f4cffd2700a2b5a172
skiliushyk <skiliushyk@gmail.com>
2021-04-27 13:30:23 +0300

    Minor changes

--- internal/server/dto/auth.go
+++ internal/server/dto/auth.go
@@ -3,7 +3,7 @@ package dto
 type SignUpRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
-	Email    string `json:"email"`
+	// Email    string `json:"email"`
 	DeviceID string `json:"deviceId"`
 }
 
